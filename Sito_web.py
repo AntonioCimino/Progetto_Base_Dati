f7b0e8c6dd6cc899a8e3716af5b091a43a54c058
Raffaella9669 <63780832+Raffaella9669@users.noreply.github.com>
2021-07-20 15:10:52 +0200

    Update Sito_web.py
    d

--- Sito_web.py
+++ Sito_web.py
@@ -5,12 +5,8 @@ import Query
 class attr:
     json_author = ""
     i = 0
-    json_country = ""
-    j = 0
-    json_len = ""
+    json_site = ""
     m = 0
-    json_site  = ""
-    n = 0
 
 class News():
   def __init__(self, i, news):
@@ -57,40 +53,34 @@ app = Flask(__name__)
 def main():
     col = Dataset_connection.con()
 
+    #Creazione grafici
     #Query.query_all_country_count()
     #Query.query_all_len_count()
 
-    list_author = Query.query_all_author()
-    #list_country = Query.query_all_country()
-    list_site = Query.query_all_site()
-    #list_len = Query.query_all_leng()
+    #Generazione indici
+    col.create_index([("author", -1)])  # INDEX
+    col.create_index([("site_url", -1)])  # INDEX
+    col.create_index([("published", -1)])  # INDEX
 
+    #Caricamento dati di aggregazione
+    list_author = Query.query_all_author()
+    list_site = Query.query_all_site()
+
+    #Creazione oggetti json
     attr.json_author = "{ \"Author\" :["
     for x in list_author:
         attr.json_author = attr.json_author + "{\"_id\" : \"" + x["_id"] + "\"},"
         attr.i = attr.i + 1
         if attr.i == 30: break
     attr.json_author = attr.json_author[0:(len(attr.json_author) - 1)] + "]}"
-    '''
-    attr.json_country = "{ \"Country\" :["
-    for x in list_country:
-        attr.json_country = attr.json_country + "{\"_id\" : \"" + x["_id"] + "\"},"
-        attr.j = attr.j + 1
-    attr.json_country = attr.json_country[0:(len(attr.json_country) - 1)] + "]}"
-    '''
+
     attr.json_site = "{ \"Site\" :["
     for x in list_site:
         attr.json_site = attr.json_site + "{\"_id\" : \"" + x["_id"] + "\"},"
         attr.m = attr.m + 1
     attr.json_site = attr.json_site[0:(len(attr.json_site) - 1)] + "]}"
-    '''
-    attr.json_len = "{ \"Len\" :["
-    for x in list_len:
-        attr.json_len = attr.json_len + "{\"_id\" : \"" + x["_id"] + "\"},"
-        attr.n = attr.n + 1
-    attr.json_len = attr.json_len[0:(len(attr.json_len) - 1)] + "]}"
-    '''
-    list = Query.query_six_recent()
+
+    list = Query.query_three_recent()
     news = []
     i = 0
     for x in list:
@@ -101,7 +91,7 @@ def main():
 
 @app.route('/Home')
 def home():
-    list = Query.query_six_recent()
+    list = Query.query_three_recent()
     news = []
     i = 0
     for x in list:
@@ -112,17 +102,8 @@ def home():
 
 @app.route('/Ricerca', methods=['GET', 'POST'])
 def ricerca():
-    col = Dataset_connection.con()
-
-    #col.create_index([("language", -1)])  # INDEX
-    col.create_index([("author", -1)])  # INDEX
-    #col.create_index([("country", -1)])  # INDEX
-    col.create_index([("published", -1)])  # INDEX
-
     resp = make_response(render_template('Ricerca.html',list_author = attr.json_author, len_author = attr.i ,
-                                         list_country = attr.json_country, len_country = attr.j,
-                                         list_site = attr.json_site, len_site = attr.m,
-                                         list_len = attr.json_len, len_len = attr.n))
+                                        list_site = attr.json_site, len_site = attr.m))
     return resp
 
 
@@ -136,31 +117,50 @@ def ricerca_risultato():
     if(tipo_ricerca == "ricerca_text"):
         testo = request.form['titolo']
         list_title, list_text = Query.query_for_text(testo)
+
+        for x in list_text:
+            news.append(News(i, x))
+            i = i + 1
+        for x in list_title:
+            news.append(News(i, x))
+            i = i + 1
+        jsonN = "{ \"News\" :" + json.dumps([z.dump() for z in news]) + "}"
+        resp = make_response(render_template('Risultati_ricerca.html', list_news=jsonN, len=len(news)))
+        return resp
+
     if(tipo_ricerca == "ricerca_date"):
         testo = request.form['date2']
-        list_title, list_text = Query.query_for_date(testo)
+        list_title = Query.query_for_date(testo)
+
+        for x in list_title:
+            news.append(News(i, x))
+            i = i + 1
+        jsonN = "{ \"News\" :" + json.dumps([z.dump() for z in news]) + "}"
+        resp = make_response(render_template('Risultati_ricerca.html', list_news=jsonN, len=len(news)))
+        return resp
+
     if(tipo_ricerca == "ricerca_author"):
         testo = request.form['author']
-        list_title, list_text = Query.query_for_author(testo)
-    if(tipo_ricerca == "ricerca_len"):
-        testo = request.form['len']
-        list_title, list_text = Query.query_for_leng(testo)
-    if(tipo_ricerca == "ricerca_country"):
-        testo = request.form['country']
-        list_title, list_text = Query.query_for_country(testo)
+        list_title = Query.query_for_author(testo)
+
+        for x in list_title:
+            news.append(News(i, x))
+            i = i + 1
+        jsonN = "{ \"News\" :" + json.dumps([z.dump() for z in news]) + "}"
+        resp = make_response(render_template('Risultati_ricerca.html', list_news=jsonN, len=len(news)))
+        return resp
+
     if(tipo_ricerca == "ricerca_site"):
         testo = request.form['site']
-        list_title, list_text = Query.query_for_site(testo)
+        list_title = Query.query_for_site(testo)
 
-    for x in list_text:
-        news.append(News(i,x))
-        i = i + 1
-    for x in list_title:
-        news.append(News(i,x))
-        i = i + 1
-    jsonN = "{ \"News\" :" + json.dumps([z.dump() for z in news]) + "}"
-    resp = make_response(render_template('Risultati_ricerca.html',list_news = jsonN, len = len(news)))
-    return resp
+        for x in list_title:
+            news.append(News(i, x))
+            i = i + 1
+        jsonN = "{ \"News\" :" + json.dumps([z.dump() for z in news]) + "}"
+        resp = make_response(render_template('Risultati_ricerca.html', list_news=jsonN, len=len(news)))
+        return resp
+
 
 @app.route('/Statistica', methods=['GET', 'POST'])
 def stat():
@@ -170,7 +170,6 @@ def stat():
 @app.route('/Team', methods=['GET', 'POST'])
 def team():
     return render_template('Team.html')
-
 
 
 if __name__ == '__main__':
